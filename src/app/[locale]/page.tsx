52309df7d2ed0665de2bb05bb17b432ecb902514
Adlen CHERIF <adlencherif29@gmail.com>
2024-12-27 13:15:19 +0100

    feat: Update French translations for HomePage content and structure

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,41 +6,35 @@ import { ArrowRight, Utensils } from "lucide-react";
 
 export default function HomePage() {
   const t = useTranslations("HomePage");
+  
   return (
-    // <div>
-    //   <h1>{t("title")}</h1>
-    //   <Link href="/about">{t("about")}</Link>
-    // </div>
     <div className="fade-bottom overflow-hidden pb-0 sm:pb-0 md:pb-0">
       <div className="mx-auto flex max-w-container flex-col gap-12 sm:gap-24">
         <div className="flex flex-col items-center gap-6 text-center sm:gap-12">
           <Badge variant="outline" className="animate-appear">
-            <span className="text-muted-foreground">
-              Le projet est 100% open-source
-            </span>
+            <span className="text-muted-foreground">{t("badge.title")}</span>
             <a href="/" className="flex items-center gap-1 ml-1">
-              En savoir plus
+              {t("badge.link")}
               <ArrowRight className="h-3 w-3" />
             </a>
           </Badge>
           <h1 className="relative z-10 inline-block animate-appear bg-gradient-to-r from-foreground to-muted-foreground bg-clip-text text-4xl font-semibold leading-tight text-transparent drop-shadow-2xl sm:text-6xl sm:leading-tight md:text-8xl md:leading-tight">
-            Vos repas, <br /> simples et accessibles.
+            {t("title.first")} <br /> {t("title.second")}
           </h1>
           <p className="text-md relative z-10 max-w-[550px] animate-appear font-medium text-muted-foreground delay-100 sm:text-xl">
-            CROUStillant vous permet de consulter les menus des restaurants
-            CROUS de France et d'outre-mer.
+            {t("subtitle")}
           </p>
           <div className="relative z-10 flex animate-appear justify-center gap-4 delay-300">
             <div className="relative z-10 flex animate-appear justify-center gap-4 delay-300">
               <Button variant="default" size="lg" asChild className="group">
                 <Link href="/restaurants">
-                  Découvrir votre menu
+                  {t("cta.first")}
                   <ArrowRight className="ml-2 h-4 w-4 group-hover:translate-x-1 transition-transform" />
                 </Link>
               </Button>
               <Button variant="outline" size="lg" asChild>
                 <Link href="/">
-                  Voir les restaurants
+                  {t("cta.second")}
                   <Utensils className="ml-2 h-4 w-4" />
                 </Link>
               </Button>
