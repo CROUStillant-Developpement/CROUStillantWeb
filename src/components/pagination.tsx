3984bb51a1f629679f5093c327992281641cc4f9
Adlen CHERIF <adlencherif29@gmail.com>
2024-12-30 18:19:13 +0100

    fix: Add boundary checks for current page in pagination component

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -86,6 +86,16 @@ export default function PaginationComponent({
     return range;
   };
 
+  useEffect(() => {
+    if (totalPages <= 0) return;
+    
+    if (currentPage > totalPages) {
+      onPageChange(totalPages);
+    } else if (currentPage < 1) {
+      onPageChange(1);
+    }
+  }, [currentPage, totalPages]);
+
   const visiblePages = getVisiblePages(currentPage, totalPages);
 
   return (
