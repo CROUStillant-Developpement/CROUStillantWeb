c2bad700d3a0721d210c8b9ab67d113308bf1a9c
Paul Bayfield <137620008+PaulBayfield@users.noreply.github.com>
2025-01-24 21:35:12 +0100

    fix: Typo favorite -> favourite

--- src/store/userPreferencesStore.ts
+++ src/store/userPreferencesStore.ts
@@ -2,7 +2,7 @@ import { DisplayType, Region } from "@/services/types";
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 
-export interface LocalStorageFavorite {
+export interface LocalStoragefavourite {
   code: number;
   name: string;
 }
@@ -12,14 +12,14 @@ export interface LocalStorageFavorite {
  */
 interface StoreState {
   /**
-   * List of favorite items stored in local storage.
+   * List of favourite items stored in local storage.
    */
-  favorites: LocalStorageFavorite[];
+  favourites: LocalStoragefavourite[];
 
   /**
-   * The favorite item that is starred.
+   * The favourite item that is starred.
    */
-  starredFav: LocalStorageFavorite | null;
+  starredFav: LocalStoragefavourite | null;
 
   /**
    * The display type setting.
@@ -27,9 +27,9 @@ interface StoreState {
   display: DisplayType;
 
   /**
-   * The region selected as favorite.
+   * The region selected as favourite.
    */
-  favoriteRegion: Region | null;
+  favouriteRegion: Region | null;
 
   /**
    * Indicates whether the dyslexic font is enabled.
@@ -47,26 +47,26 @@ interface StoreState {
   toggleDislexicFont: () => void;
 
   /**
-   * Adds or removes an item from the favorites list.
+   * Adds or removes an item from the favourites list.
    * 
    * @param code - The code of the item.
    * @param name - The name of the item (optional).
    */
-  addOrRemoveFromFavorites: (code: number, name?: string, qr?: boolean) => void;
+  addOrRemoveFromfavourites: (code: number, name?: string, qr?: boolean) => void;
 
   /**
-   * Sets the starred favorite item.
+   * Sets the starred favourite item.
    * 
-   * @param favorite - The favorite item to be starred.
+   * @param favourite - The favourite item to be starred.
    */
-  setStarredFav: (favorite: LocalStorageFavorite) => void;
+  setStarredFav: (favourite: LocalStoragefavourite) => void;
 
   /**
-   * Sets the favorite region.
+   * Sets the favourite region.
    * 
-   * @param region - The region to be set as favorite.
+   * @param region - The region to be set as favourite.
    */
-  setFavoriteRegion: (region: Region) => void;
+  setfavouriteRegion: (region: Region) => void;
 
   /**
    * Clears all user preferences.
@@ -81,31 +81,31 @@ interface StoreState {
  * @returns {StoreState} The state and actions to manage user preferences.
  *
  * @property {string} display - The current display mode, either "list" or "map".
- * @property {Array<{code: number, name: string}>} favorites - The list of favorite items.
- * @property {null | {code: number, name: string}} starredFav - The starred favorite item.
- * @property {{code: number, libelle: string}} favoriteRegion - The user's favorite region.
+ * @property {Array<{code: number, name: string}>} favourites - The list of favourite items.
+ * @property {null | {code: number, name: string}} starredFav - The starred favourite item.
+ * @property {{code: number, libelle: string}} favouriteRegion - The user's favourite region.
  * @property {boolean} dislexicFont - Whether the dyslexic font is enabled.
  *
  * @method toggleDisplay - Toggles the display mode between "list" and "map".
  * @method toggleDislexicFont - Toggles the dyslexic font setting.
- * @method addOrRemoveFromFavorites - Adds or removes an item from the favorites list.
+ * @method addOrRemoveFromfavourites - Adds or removes an item from the favourites list.
  * @param {number} code - The code of the item to add or remove.
  * @param {string} [name] - The name of the item to add (optional).
  * @param {number} code - The code of the item to add.
  * @param {string} name - The name of the item to add.
- * @method setStarredFav - Sets the starred favorite item.
- * @param {LocalStorageFavorite} favorite - The favorite item to set as starred.
- * @method setFavoriteRegion - Sets the user's favorite region.
- * @param {Region} region - The region to set as favorite.
+ * @method setStarredFav - Sets the starred favourite item.
+ * @param {LocalStoragefavourite} favourite - The favourite item to set as starred.
+ * @method setfavouriteRegion - Sets the user's favourite region.
+ * @param {Region} region - The region to set as favourite.
  * @method clearUserPreferences - Clears all user preferences, resetting to default values.
  */
 export const useUserPreferences = create<StoreState>()(
   persist(
     (set) => ({
       display: "list",
-      favorites: [],
+      favourites: [],
       starredFav: null,
-      favoriteRegion: { code: -1, libelle: "All Regions" },
+      favouriteRegion: { code: -1, libelle: "All Regions" },
       dislexicFont: false,
 
       toggleDisplay: () =>
@@ -118,24 +118,24 @@ export const useUserPreferences = create<StoreState>()(
           dislexicFont: !state.dislexicFont,
         })),
 
-      addOrRemoveFromFavorites: (code: number, name?: string, qr: boolean = false) =>
+      addOrRemoveFromfavourites: (code: number, name?: string, qr: boolean = false) =>
         set((state) => {
-          const index = state.favorites.findIndex((f) => f.code === code);
+          const index = state.favourites.findIndex((f) => f.code === code);
 
-          // if index is -1, the item is not in the favorites list
+          // if index is -1, the item is not in the favourites list
           if (index === -1) {
             if (!state.starredFav) {
               return {
                 starredFav: { code, name: name ?? "Unknown" },
-                favorites: [
-                  ...state.favorites,
+                favourites: [
+                  ...state.favourites,
                   { code, name: name ?? "Unknown" },
                 ],
               };
             }
             return {
-              favorites: [
-                ...state.favorites,
+              favourites: [
+                ...state.favourites,
                 { code, name: name ?? "Unknown" },
               ],
             };
@@ -144,38 +144,38 @@ export const useUserPreferences = create<StoreState>()(
           // if qr is true, we don't want to remove the item from the list (if called 2 times in a row)
           if (qr) {
             return {
-              favorites: state.favorites,
+              favourites: state.favourites,
             };
           }
 
-          const newFavorites = [...state.favorites];
-          newFavorites.splice(index, 1);
+          const newfavourites = [...state.favourites];
+          newfavourites.splice(index, 1);
 
-          if (!state.starredFav && newFavorites.length > 0) {
-            return { starredFav: newFavorites[0], favorites: newFavorites };
+          if (!state.starredFav && newfavourites.length > 0) {
+            return { starredFav: newfavourites[0], favourites: newfavourites };
           } else if (state.starredFav?.code === code) {
-            return { starredFav: null, favorites: newFavorites };
+            return { starredFav: null, favourites: newfavourites };
           } else {
-            return { favorites: newFavorites };
+            return { favourites: newfavourites };
           }
         }),
 
-      setStarredFav: (favorite: LocalStorageFavorite) =>
+      setStarredFav: (favourite: LocalStoragefavourite) =>
         set(() => ({
-          starredFav: favorite,
+          starredFav: favourite,
         })),
 
-      setFavoriteRegion: (region: Region) =>
+      setfavouriteRegion: (region: Region) =>
         set(() => ({
-          favoriteRegion: region,
+          favouriteRegion: region,
         })),
 
       clearUserPreferences: () =>
         set(() => ({
-          favorites: [],
+          favourites: [],
           starredFav: null,
           display: "list",
-          favoriteRegion: { code: -1, libelle: "All Regions" },
+          favouriteRegion: { code: -1, libelle: "All Regions" },
         })),
     }),
     {
