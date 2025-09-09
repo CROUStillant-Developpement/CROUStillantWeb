e495c6223dbb768eafce6dd752de0b04baeb3db0
Paul Bayfield <137620008+PaulBayfield@users.noreply.github.com>
2025-09-09 08:38:36 +0200

    fix: Forgot '}'

--- src/components/restaurants/active-filter-badge.tsx
+++ src/components/restaurants/active-filter-badge.tsx
@@ -24,7 +24,7 @@ export default function ActiveFilterBadge({
         onClick={() => {
           onRemove?.();
           umami.event("Restaurant.Filter.Remove");
-        }
+        }}
       />
     </Badge>
   );
